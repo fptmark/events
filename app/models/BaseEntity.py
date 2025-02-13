e5298e43909f255bfb9f37cb699ae9552d4251ad
Mark Malamut <mark40353@gmail.com>
2025-02-13 11:56:23 -0800

    update datetime

--- app/models/BaseEntity.py
+++ app/models/BaseEntity.py
@@ -2,7 +2,7 @@
 from beanie import Document, PydanticObjectId
 from pydantic import Field
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timezone
 
 class BaseEntity(Document):
     """
@@ -12,12 +12,12 @@ class BaseEntity(Document):
 
     _id: Optional[PydanticObjectId] = Field(default_factory=PydanticObjectId)
 
-    createdAt: datetime = Field(default_factory=datetime.utcnow)
+    createdAt: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     updatedAt: Optional[datetime] = Field(None)
 
     async def save(self, *args, **kwargs):
-        self.updatedAt = datetime.utcnow()
+        self.updatedAt = datetime.now(timezone.utc)
         return await super().save(*args, **kwargs)
 
     class Settings:
