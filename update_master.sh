d137d21938e44b990b35d72572a85d8af3ada1bf
Mark Malamut <mark40353@gmail.com>
2025-07-25 16:07:13 -0700

    update server generic files from events (sample app)

--- update_master.sh
+++ update_master.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-cp Makefile ../schema2rest/src/server_generic_files
 
+# base infrastructure files
+cp Makefile ../schema2rest/src/server_generic_files
+cp requirements.txt ../schema2rest/src/server_generic_files
+
+# base app files
 cp app/*.py ../schema2rest/src/server_generic_files
 rm ../schema2rest/src/server_generic_files/main.py
 
+# db and router is app independent
 cp -r app/db ../schema2rest/src/server_generic_files
 cp -r app/routers ../schema2rest/src/server_generic_files
 
+# model files that are app independend
+cp -r app/models/list_params.py ../schema2rest/src/server_generic_files/models
+
+# cleanup pycache
 find ../schema2rest/src/server_generic_files -name '__pycache__' -exec rm -rf {} \;
+
+# ls the files
 ls ../schema2rest/src/server_generic_files
 ls ../schema2rest/src/server_generic_files/db
 ls ../schema2rest/src/server_generic_files/routers
+ls ../schema2rest/src/server_generic_files/models
+
+# show tree of the files
 tree ../schema2rest/src/server_generic_files
