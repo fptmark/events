f8431a75157c99c3fe3a66735cbdd2a0480af663
Mark Malamut <mark40353@gmail.com>
2025-10-08 17:38:25 -0700

    upgraded test framework

--- test/data-src/pkg/database/operations.go
+++
@@ -1,4 +0,0 @@
-package database
-
-// This package is kept for backwards compatibility but no longer uses database drivers.
-// All operations now go through the server API via the generator package.
